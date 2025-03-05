678f8259cebdbe644c979a51bf1eb08d24ac50c9
haidarsatria <haidarsatria72@gmail.com>
2025-03-05 18:44:47 +0700

    fixing little mistake

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  print_r($mahasiswaTI->getData());
  $mahasiswaSI = new Mahasiswa();
  $mahasiswaSI->setData('A12.2000.00001','Junior','Sistem Informasi');
+ print_r($mahasiswaTI->getData());
 
  ?>
 
